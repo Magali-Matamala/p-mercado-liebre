8b949669e22aeb29fc1ee6dc133064d2322e60bb
magali matamala <magalimelisamatamala@outlook.com>
2023-01-02 20:56:14 -0300

    actualización render

--- views/app.js
+++ views/app.js
@@ -7,9 +7,11 @@ const rutaPublic = path.join(__dirname, '../public');
 const static = express.static(rutaPublic);
 app.use(static)
 
-app.listen(3030,()=>{
-    console.log('Servidor funcionando')
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor corriendo en el puerto ${PORT}`);
 })
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'home.html'))
 })
